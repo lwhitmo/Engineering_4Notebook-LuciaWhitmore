89e2247c3f5ee1b01495756ae29333a8c62ac780
lwhitmo <lwhitmo03@charlottesvilleschools.org>
2023-09-07 14:03:51 -0400

    countdown launch

---
+++ raspberry-pi/launchpad.py
@@ -0,0 +1,12 @@
+#type: ignore
+
+import time
+import board
+import digitalio
+redled = digitalio.DigitalInOut(board.LED)
+redled.direction = digitalio.Direction.OUTPUT 
+
+for x in range(10,0):
+  print(x)
+time.sleep(1)
+
