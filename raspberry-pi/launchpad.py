cda5f625ae2ce46fffb2897c3c49629cfdb19834
lwhitmo <lwhitmo03@charlottesvilleschools.org>
2023-09-08 13:58:54 -0400

    countdown launchpad

--- raspberry-pi/launchpad.py
+++ raspberry-pi/launchpad.py
@@ -7,7 +7,9 @@ redled = digitalio.DigitalInOut(board.LED)
 redled.direction = digitalio.Direction.OUTPUT 
 
 
-for x in range(10,0):
+for x in range(10,0,-1):
    print(x)
+   time.sleep(1)
 
 
+print("Liftoff!") 
