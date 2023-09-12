e6399d821b976ca99f4b5a0350c7c8c6e3b04caf
lwhitmo <lwhitmo03@charlottesvilleschools.org>
2023-09-12 14:05:00 -0400

    butonnnn

---
+++ raspberry-pi/launchpadPt3 button.py
@@ -0,0 +1,22 @@
+#type: ignore
+
+import time
+import board
+import digitalio
+redled = digitalio.DigitalInOut(board.GP12)
+redled.direction = digitalio.Direction.OUTPUT 
+greenLED =digitalio.DigitalInOut(board.GP19)
+greenLED.direction = digitalio.Direction.OUTPUT 
+
+for x in range(10,0,-1):
+   print(x)
+   redled.value = True
+   time.sleep(.5)
+   redled.value = False
+   time.sleep(.5)
+
+
+
+print("Liftoff!") 
+greenLED.value = True
+time.sleep(3)
