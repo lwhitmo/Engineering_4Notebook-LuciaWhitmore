bab3540758086e78b1badb52ef1def42a5e43c28
lwhitmo <lwhitmo03@charlottesvilleschools.org>
2023-09-13 13:50:28 -0400

    button code

--- raspberry-pi/launchpadPt3 button.py
+++ raspberry-pi/launchpadPt3 button.py
@@ -2,21 +2,31 @@
 
 import time
 import board
-import digitalio
+import digitalio 
+button = digitalio.DigitalInOut(board.GP0)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.DOWN
 redled = digitalio.DigitalInOut(board.GP12)
 redled.direction = digitalio.Direction.OUTPUT 
 greenLED =digitalio.DigitalInOut(board.GP19)
 greenLED.direction = digitalio.Direction.OUTPUT 
 
-for x in range(10,0,-1):
-   print(x)
-   redled.value = True
-   time.sleep(.5)
-   redled.value = False
-   time.sleep(.5)
+while button.value == False:
+   
+   if button.value == True:
+
+      for x in range(10,0,-1):
+         print(x)
+         redled.value = True
+         time.sleep(.5)
+         redled.value = False
+         time.sleep(.5)
+      
+      print("Liftoff!") 
+      greenLED.value = True
+      time.sleep(3)
 
 
 
-print("Liftoff!") 
-greenLED.value = True
-time.sleep(3)
+
+
