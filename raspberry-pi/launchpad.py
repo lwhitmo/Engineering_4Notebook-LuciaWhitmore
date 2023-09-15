a0aad4bd6cdad8223e600eff23b5e6bb9e8b60be
lwhitmo <112962114+lwhitmo@users.noreply.github.com>
2023-09-15 13:56:43 -0400

    Update launchpad.py

--- raspberry-pi/launchpad.py
+++ raspberry-pi/launchpad.py
@@ -1,15 +1,20 @@
+# The following line is a comment that indicates the type checking tool to ignore this file.
 #type: ignore
+# This can be useful when using tools like mypy for static type checking.
 
-import time
-import board
-import digitalio
-redled = digitalio.DigitalInOut(board.LED)
-redled.direction = digitalio.Direction.OUTPUT 
+# Import necessary modules
+import time  # Provides time-related functions
+import board  # Provides access to hardware pins on microcontrollers
+import digitalio  # Provides digital input/output capabilities
 
+# Initialize the red LED
+redled = digitalio.DigitalInOut(board.LED)  # Create a DigitalInOut object for the built-in LED
+redled.direction = digitalio.Direction.OUTPUT  # Set the direction of the LED pin to OUTPUT
 
-for x in range(10,0,-1):
-   print(x)
-   time.sleep(1)
+# Countdown loop from 10 to 1
+for x in range(10, 0, -1):
+    print(x)  # Print the current value of x
+    time.sleep(1)  # Pause for 1 second
 
-
-print("Liftoff!") 
+# After the countdown, print "Liftoff!"
+print("Liftoff!")
