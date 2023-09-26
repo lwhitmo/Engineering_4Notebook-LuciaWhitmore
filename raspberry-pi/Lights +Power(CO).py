6e87ce444dbc7a9f9e2512455879c8a37e2d6b25
lwhitmo <lwhitmo03@charlottesvilleschools.org>
2023-09-26 13:25:29 -0400

    CO

--- raspberry-pi/Lights +Power(CO).py
+++ raspberry-pi/Lights +Power(CO).py
@@ -1,4 +1,5 @@
-#type: ignore
+# type: ignore 
+# comment: This is a comment that tells the type checker to ignore type checking for this script.
 
 # Import necessary libraries
 import time  # Provides time-related functions
@@ -8,14 +9,18 @@ import adafruit_mpu6050  # Provides access to the MPU6050 sensor
 import busio  # Provides support for I2C communication
 
 # Define the pins for I2C communication
-sda_pin = board.GP14  # Assign the SDA pin for I2C communication
-scl_pin = board.GP15  # Assign the SCL pin for I2C communication
+sda_pin = board.GP14  # SDA pin for I2C communication
+scl_pin = board.GP15  # SCL pin for I2C communication
 
 # Initialize the I2C bus using the specified pins
 i2c = busio.I2C(scl_pin, sda_pin)
 
 # Create an instance of the MPU6050 sensor using the I2C bus
 mpu = adafruit_mpu6050.MPU6050(i2c)
+
+# Initialize a red LED connected to GP12 for output
+redled = digitalio.DigitalInOut(board.GP12)
+redled.direction = digitalio.Direction.OUTPUT 
 
 # Infinite loop for continuously reading and processing data
 while True:
@@ -27,6 +32,12 @@ while True:
     # Print the accelerometer data
     print(f" x Acceleration:{x} \n y Acceleration:{y} \n z Acceleration:{z}")
     print("")
+
+    # Check if the z-axis acceleration is less than or equal to 2
+    if z <= 2:
+        redled.value = True  # Turn on the red LED if the condition is met
+    else:
+        redled.value = False  # Turn off the red LED if the condition is not met
 
     # Pause execution for 1 second
     time.sleep(1)
